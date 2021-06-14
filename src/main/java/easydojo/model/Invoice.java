1fab0086af0a25e0e2d04764cc2ce692c2745594
Lingrui <392425595@qq.com>
2021-06-14 11:39:07 +0800

    init refactoring quiz: performance statement

---
+++ src/main/java/easydojo/model/Invoice.java
@@ -0,0 +1,26 @@
+package easydojo.model;
+
+import java.util.List;
+
+public class Invoice {
+
+    private String customer;
+
+    private List<Performance> performances;
+
+    public String getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(String customer) {
+        this.customer = customer;
+    }
+
+    public List<Performance> getPerformances() {
+        return performances;
+    }
+
+    public void setPerformances(List<Performance> performances) {
+        this.performances = performances;
+    }
+}
